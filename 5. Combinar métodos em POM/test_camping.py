cc94e86a5ae1565e09f40eab91a1b1e65a8707ca
RoberthGuimaro <guimaroroberth@gmail.com>
2025-08-17 20:43:42 -0400

    task 05 completed (test_camping.py and urban_routes_main_page.py)

--- 5. Combinar métodos em POM/test_camping.py
+++ 5. Combinar métodos em POM/test_camping.py
@@ -7,7 +7,7 @@ class TestUrbanRoutes:
 
     def test_carsharing_personal_camping_option(self):
         driver = webdriver.Chrome()
-        driver.get('https://cnt-784b233b-3492-4228-8423-83e3047817c1.containerhub.tripleten-services.com')
+        driver.get('https://cnt-1a8b9491-02bc-4e9d-afe3-6ca1bace1742.containerhub.tripleten-services.com?lng=pt')
 
         # Crie uma instância da classe de página
         urban_routes_page = UrbanRoutesPage(driver)
@@ -17,7 +17,9 @@ class TestUrbanRoutes:
 
        # Escolha a etapa do carro de camping para inserir "De", "Para" e clicar em "personal_option",
         # "carsharing_icon", "book_button", e "camping"
-        ...
+        urban_routes_page.choose_camping_car("East 2nd Street, 601", "1300 1St st")
+
+        driver.implicitly_wait(3)
 
         # Verifique se o texto exibe "Audi A3 Sedã"
         actual_value = urban_routes_page.get_audi_text()
@@ -27,7 +29,7 @@ class TestUrbanRoutes:
 
     def test_add_driver_license_personal_camping_option(self):
         driver = webdriver.Chrome()
-        driver.get('https://cnt-784b233b-3492-4228-8423-83e3047817c1.containerhub.tripleten-services.com')
+        driver.get('https://cnt-1a8b9491-02bc-4e9d-afe3-6ca1bace1742.containerhub.tripleten-services.com?lng=pt')
 
         # Crie uma instância da classe de página
         urban_routes_page = UrbanRoutesPage(driver)
@@ -36,15 +38,16 @@ class TestUrbanRoutes:
 
        # Escolha a etapa do carro de camping para inserir "De", "Para" e clicar em "personal_option",
         # "carsharing_icon", "book_button", e "camping"
-        ...
+        urban_routes_page.choose_camping_car("East 2nd Street, 601", "1300 1St st")
 
         # Adicionar a etapa de carteira de motorista para clicar em "adding_driver_license";
         # para inserir "first_name", "last_name", "date_of_birth", "number"; e
         # para clicar em "title" e "add_button"
-        ...
+        urban_routes_page.adding_driver_license("Anna", "Smith", "10051996", "0101123456")
 
         # Verifique se a licença foi adicionada
         actual_value = urban_routes_page.get_verification_text()
         expected_value = "Obrigado!"
         assert expected_value in actual_value, f"Esperado '{expected_value}', mas recebeu '{actual_value}'"
+        print("O teste passou")
         driver.quit()
--- 5. Combinar métodos em POM/urban_routes_main_page.py
+++ 5. Combinar métodos em POM/urban_routes_main_page.py
@@ -1,13 +1,16 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 
-# Definição da classe da página, dos localizadores e do método na classe
+# Definição da classe da página, dos localizadores e do metodo na classe
 class UrbanRoutesPage:
     # Localizadores como atributos de classe
     FROM_LOCATOR = (By.ID, 'from')
     TO_LOCATOR = (By.ID, 'to')
     PERSONAL_OPTION_LOCATOR = (By.XPATH, '//div[text()="Personal"]')
-    CARSHARING_ICON_LOCATOR = (By.XPATH, '(//img[@src="/static/media/car.8a2b1ff5.svg"])[2]')
+    CARSHARING_ICON_LOCATOR = (By.XPATH, '//img[@src="/static/media/drive.05beabd2.svg"]')
+    #CARSHARING_ICON_LOCATOR = (By.XPATH, '(//img[@src="/static/media/car.8a2b1ff5.svg"])[2]')
     BOOK_BUTTON_LOCATOR = (By.XPATH, '//button[@class="button round"]')
     CAMPING_LOCATOR = (By.XPATH, '//div[contains(text(),"Camping")]')
     AUDI_TEXT_LOCATOR = (By.XPATH, '//div[contains(text(),"Audi A3 Sedã")]')
@@ -23,71 +26,32 @@ class UrbanRoutesPage:
     def __init__(self, driver):
         self.driver = driver  # Inicializar o driver
 
-    def enter_from_location(self, from_text):
+    def choose_camping_car(self, from_text, to_text):
         # Inserir De
         self.driver.find_element(*self.FROM_LOCATOR).send_keys(from_text)
-
-    def enter_to_location(self, to_text):
-        # Inserir Para
         self.driver.find_element(*self.TO_LOCATOR).send_keys(to_text)
-
-    def click_personal_option(self):
-        # Clicar Personal
         self.driver.find_element(*self.PERSONAL_OPTION_LOCATOR).click()
-
-    def click_carsharing_icon(self):
-        # Clique no ícone Carsharing
+        time.sleep(1)
         self.driver.find_element(*self.CARSHARING_ICON_LOCATOR).click()
-
-    def click_book_button(self):
-        # Clique no botão Reservar
         self.driver.find_element(*self.BOOK_BUTTON_LOCATOR).click()
-
-    def click_camping(self):
-        # Clique em Camping
+        time.sleep(1)
         self.driver.find_element(*self.CAMPING_LOCATOR).click()
 
     def get_audi_text(self):
-         # Retornar o texto "Audi"
+        #Retornar o texto "Audi"
         return self.driver.find_element(*self.AUDI_TEXT_LOCATOR).text
-
-    def click_add_driver_license(self):
-        # Clicar em Adicionar carteira de motorista
-        self.driver.find_element(*self.ADD_DRIVER_LICENSE_LOCATOR).click()
-
-    def enter_first_name(self, first_name):
-        # Digitar Nome
-        self.driver.find_element(*self.FIRST_NAME_LOCATOR).send_keys(first_name)
-
-    def enter_last_name(self, last_name):
-        # Digitar Sobrenome
-        self.driver.find_element(*self.LAST_NAME_LOCATOR).send_keys(last_name)
-
-    def enter_date_of_birth(self, date_of_birth):
-        # Inserir Data de nascimento
-        self.driver.find_element(*self.DATE_OF_BIRTH_LOCATOR).send_keys(date_of_birth)
-
-    def enter_number(self, number):
-        # Digitar Número
-        self.driver.find_element(*self.NUMBER_LOCATOR).send_keys(number)
-
-    def click_title(self):
-        # Clicar Adicionar um título de carteira de motorista
-        self.driver.find_element(*self.ADD_DRIVER_LICENSE_TITLE_LOCATOR).click()
-
-    def click_add_button(self):
-        # Clicar no botão Adicionar
-        self.driver.find_element(*self.ADD_BUTTON_LOCATOR).click()
-
-    def get_verification_text(self):
-        # Retornar o texto de verificação
-        return self.driver.find_element(*self.VERIFICATION_TEXT_LOCATOR).text
-
-    # Etapa para inserir "De", "Para" e clicar em "personal_option", "carsharing_icon", "book_button" e "camping"
-    def choose_camping_car(self, from_text, to_text):
-        ...
 
     # Etapa para clicar em "add_driver_license"; para digitar "first_name", "last_name", "date_of_birth", "number"; e
     # para clicar em "title" e "add_button"
     def adding_driver_license(self, first_name, last_name, date_of_birth, number):
-        ...
+        self.driver.find_element(*self.ADD_DRIVER_LICENSE_LOCATOR).click()
+        self.driver.find_element(*self.FIRST_NAME_LOCATOR).send_keys(first_name)
+        self.driver.find_element(*self.LAST_NAME_LOCATOR).send_keys(last_name)
+        self.driver.find_element(*self.DATE_OF_BIRTH_LOCATOR).send_keys(date_of_birth)
+        self.driver.find_element(*self.NUMBER_LOCATOR).send_keys(number)
+        self.driver.find_element(*self.ADD_DRIVER_LICENSE_TITLE_LOCATOR).click()
+        self.driver.find_element(*self.ADD_BUTTON_LOCATOR).click()
+
+    def get_verification_text(self):
+        #Retornar o texto de verificação
+        return self.driver.find_element(*self.VERIFICATION_TEXT_LOCATOR).text
